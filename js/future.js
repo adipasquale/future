cd9a46936f6f188da38bddaf341d3843db7a7535
adipasquale <adipasquale@users.noreply.github.com>
2023-09-05 17:01:32 +0000

    deploy: 0ba55110f2679f796d07283acc84e59b7a447476

--- js/future.js
+++ js/future.js
@@ -1,10 +1,24 @@
 // on document ready without jquery
+import PhotoSwipeLightbox from '/js/photoswipe-lightbox.esm.js';
+
 document.addEventListener("DOMContentLoaded", function (event) {
   const toggleMenu = () => {
-    document.querySelectorAll("main nav, .page-title").forEach(elt => elt.classList.toggle("hide"))
+    document.querySelectorAll("header nav.menu, .page-title").forEach(elt => elt.classList.toggle("hide"))
   }
-  document.querySelector("header nav a").addEventListener("click", event => {
+  document.querySelector("header nav.menu-link a").addEventListener("click", event => {
     event.preventDefault()
     toggleMenu()
   })
+
+  if (document.querySelector("a.photoswipe-item")) {
+    const lightbox = new PhotoSwipeLightbox({
+      gallery: 'body',
+      children: 'a.photoswipe-item',
+      pswpModule: () => import('/js/photoswipe.esm.js'),
+      showHideAnimationType: 'none',
+      zoomAnimationDuration: 0,
+      preloaderDelay: 0
+    });
+    lightbox.init();
+  }
 })
